b2777c1200756b7fdef946cfbff35249d67bdfa3
deepravi0522 <deepravi0522@gmail.com>
2023-02-28 00:29:20 +0530

    first commit

---
+++ 9. Palindrome Number/Palindrome_Number.java
@@ -0,0 +1,23 @@
+/**
+ * Palindrome_Number
+ */
+public class Palindrome_Number {
+    public static void main(String[] args) {
+        System.out.print("Enter number to check Palindrome: ");
+        int num = System.in();
+
+        Boolean isPalindrome = isPalindrome(num);
+
+
+
+    }
+
+    static boolean isPalindrome(int num) {
+
+        
+
+        return false;
+
+    }
+
+}
