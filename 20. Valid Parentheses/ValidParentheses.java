6c38564b23182697de3a8bc9e6071e1076a1f1d1
Deepankar Anand <deepravi0522@gmail.com>
2024-02-05 14:07:53 +0530

    MergeTwoSortedLists

--- 20. Valid Parentheses/ValidParentheses.java
+++ 20. Valid Parentheses/ValidParentheses.java
@@ -1,5 +1,36 @@
+import java.util.Stack;
+
 public class ValidParentheses {
     public static void main(String[] args) {
-        
+        String s = "()[{}";
+        System.out.println(isValid(s));
+    }
+
+    private static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if(stack.isEmpty() || stack.pop()!='(')
+                        return false;
+                    break;
+                case '}':
+                    if(stack.isEmpty() || stack.pop()!='{')
+                        return false;
+                    break;
+                case ']':
+                    if(stack.isEmpty() || stack.pop()!='[')
+                        return false;
+                    break;
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
---
+++ 21. Merge Two Sorted Lists/MergeTwoSortedLists.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class MergeTwoSortedLists {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode listNode = new ListNode(-1);
+
+        return listNode;
+    }
+}
